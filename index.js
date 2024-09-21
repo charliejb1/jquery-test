4c3bf8f3029b7f0e18fc9d7ae782b7d1214a7b9a
Charlie <charlieboyle252@gmail.com>
2024-09-21 12:23:24 -0500

    jquery test project

---
+++ index.js
@@ -0,0 +1,20 @@
+// $('h1#heading1').hide();
+// $('h1.heading2').hide();
+
+// $('p span').css('color','red')
+
+$('#span1').css('color','red')
+
+$('ul#list li:first').css('color','red');
+$('ul#list li:nth-child(2)').css('color','green');
+$('ul#list li:last').css('color','blue');
+$('ul#list li:odd').css('font-weight','bold');
+$('ul#list').css('list-style','square');
+
+
+$('h1#heading1').css('backgroundColor','red')
+$('h1#heading1').css('width','150px')
+
+$(':text').hide();
+$('[href]').css('color','red')
+$('[href="https://www.linkedin.com/feed/"]').css('color','blue')
